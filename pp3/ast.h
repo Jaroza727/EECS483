89cc07d29bad121986e6ada0430cba8dc8c7093b
qiyuh <qiyuh@umich.edu>
2015-02-26 15:21:10 -0500

    restore ast.h

--- pp3/ast.h
+++ pp3/ast.h
@@ -46,7 +46,6 @@ class Node
     yyltype *GetLocation()   { return location; }
     void SetParent(Node *p)  { parent = p; }
     Node *GetParent()        { return parent; }
-    void Check()             {}
 };
    
 
